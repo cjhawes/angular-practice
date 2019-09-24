959c7ef1957e25e1c0d32466216779da319f23b6
cjhawes <cjhawes.cs@gmail.com>
2019-09-24 07:37:35 +0100

    Initial framework layout of site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { NavComponent } from './nav/nav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    NavComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
