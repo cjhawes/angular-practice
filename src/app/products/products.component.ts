e1e7f9f9eb5820b5a5cf23d4ccd1dbc8cf5d7b7d
Connor <cjhawes.cs@gmail.com>
2019-09-26 21:15:41 +0100

    Added products list array temporarily into the app

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,6 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.sass']
 })
 export class ProductsComponent implements OnInit {
+  pageTitle = 'Product List';
+  products: any[] = [
+    {
+      productId: 1,
+      productsName: 'Bin',
+      productCode: 'BIN01',
+      releaseDate: '21-01-2019',
+      description: 'Big ol\' barrel of nothing ready to be filled with the crap you dont want anymore.',
+      price: 2000.99,
+      starRating: 5,
+      imageUrl: 'assets/images/bin.png'
+    },
+    {
+    productId: 2,
+    productsName: 'Twig',
+    productCode: 'TWIG01',
+    releaseDate: '21-01-2019',
+    description: 'Big ol\' twig.',
+    price: 20.99,
+    starRating: 3.2,
+    imageUrl: 'assets/images/twig.png'}
+  ];
+
 
   constructor() { }
 
